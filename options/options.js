32ad5a8acc0439a308b1612aeec5706a0eabce5e
Crowdbotics <team@crowdbotics.com>
2024-03-26 20:23:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "usaf-scheduling-app-47285",
+  url: "https://usaf-scheduling-app-47285.botics.co",
+  api: "https://usaf-scheduling-app-47285.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
